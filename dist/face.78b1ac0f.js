534776374f5e486427267cdf737897731c17a6e1
vildane <vildanealiu@hotmail.com>
2024-10-09 23:10:32 +0200

    additional

--- dist/face.78b1ac0f.js
+++ dist/face.78b1ac0f.js
@@ -2,10 +2,9 @@ const video = document.querySelector(".webcam");
 const canvas = document.querySelector(".video");
 const ctx = canvas.getContext("2d");
 const faceCanvas = document.querySelector(".face");
-const faceCtx = canvas.getContext("2d");
+const faceCtx = faceCanvas.getContext("2d");
 const faceDetector = new window.FaceDetector();
-console.log(video, canvas, faceCanvas, faceDetector);
-//write a function that will populate the users video
+// Write a fucntion that will populate the users video
 async function populateVideo() {
     const stream = await navigator.mediaDevices.getUserMedia({
         video: {
@@ -13,9 +12,24 @@ async function populateVideo() {
             height: 720
         }
     });
-    video.srcObject = stream; //since is a stream you do src.Object
+    video.srcObject = stream;
     await video.play();
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    faceCanvas.width = video.videoWidth;
+    faceCanvas.height = video.videoHeight;
 }
-populateVideo();
+async function detect() {
+    const faces = await faceDetector.detect(video);
+    console.log(faces.length);
+    //ask the browser when the next animation frame(ftyra) is, and tell it to run detect for us:
+    faces.forEach(drawFace);
+    requestAnimationFrame(detect);
+}
+// draw a rectangle around the face 
+function drawFace(face) {
+    console.log(face);
+}
+populateVideo().then(detect);
 
 //# sourceMappingURL=face.78b1ac0f.js.map
--- pixelated-face.js
+++ pixelated-face.js
@@ -1,21 +1,40 @@
 const video = document.querySelector('.webcam');
-
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
-
+ctx.strokeStyle = '#ffc600';
+ctx.lineWidth = 2;
 const faceCanvas = document.querySelector('.face');
-const faceCtx = canvas.getContext('2d');
-
+const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
-console.log(video, canvas, faceCanvas, faceDetector);
 
-//write a function that will populate the users video
 
+// Write a fucntion that will populate the users video
 async function populateVideo() {
     const stream = await navigator.mediaDevices.getUserMedia({
-        video: {width: 1280, height: 720},
+      video: { width: 1280, height: 720 },
     });
-    video.srcObject = stream; //since is a stream you do src.Object
+    video.srcObject = stream;
     await video.play();
+    // size the canvases to be the same size as the video
+    console.log(video.videoWidth, video.videoHeight);
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    faceCanvas.width = video.videoWidth;
+    faceCanvas.height = video.videoHeight;
+  }
+
+
+async function detect() {
+    const faces = await faceDetector.detect(video);
+    console.log(faces.length);
+
+    //ask the browser when the next animation frame(ftyra) is, and tell it to run detect for us:
+    faces.forEach(drawFace)
+    requestAnimationFrame(detect);
 }
-populateVideo();
+// draw a rectangle around the face 
+function drawFace(face){
+  const {width, height, top} = face.boundingBox;
+  ctx.strokeRect(left, top, width, height);
+}
+populateVideo().then(detect);
